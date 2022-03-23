76dce92b4f98b94944f3fcf8bd5e369150b63421
Akinlade Temitope <atemitope95@gmail.com>
2022-03-23 21:47:18 +0100

    Fixed issue as requested

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module HelloReactBackend
 
 
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
